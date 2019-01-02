e2328ccca37ba3cb2ae1c6cb5f55f93e9bf0d3ae
Tristan Rosales <tristanrosales22@gmail.com>
2019-01-02 22:00:02 +0800

    changed EndPoint to production...

--- app/src/main/java/com/capstone/alerrt_app/classes/EndPoints.java
+++ app/src/main/java/com/capstone/alerrt_app/classes/EndPoints.java
@@ -1,8 +1,8 @@
 package com.capstone.alerrt_app.classes;
 
 public class EndPoints {
-    //public static String BASE_URL = "http://alerrt.x10.mx/ALERRT/"; //for production
-    public static String BASE_URL = "http://192.168.1.5/ALERRT/"; //for development
+    public static String BASE_URL = "http://alerrt.x10.mx/ALERRT/mobile_app/"; //for production
+    //public static String BASE_URL = "http://192.168.1.5/ALERRT/mobile_app/"; //for development
 
     public static String LOGIN_USER = BASE_URL + "loginUser.php";
     public static String REGISTER_USER = BASE_URL + "registerUser.php";
